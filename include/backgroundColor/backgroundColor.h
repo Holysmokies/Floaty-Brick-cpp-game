c342d7f3eda18282701440fa28db18c3d75a29f1
Josh Rutledge <joshua.rutledge@att.net>
2024-03-07 17:08:35 +0000

    Create backgroundColor.h

---
+++ include/backgroundColor/backgroundColor.h
@@ -0,0 +1,10 @@
+#ifndef BACKGROUND_COLOR_H
+#define BACKGROUND_COLOR_H
+
+bool bool_check(float, bool);
+
+float red_change(float, bool);
+float green_change(float, bool);
+float blue_change(float, bool);
+
+#endif

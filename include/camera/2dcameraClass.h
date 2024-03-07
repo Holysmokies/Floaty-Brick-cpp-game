5495103cfa9b2326617133d1a1ff95a1f525e7c8
Josh Rutledge <joshua.rutledge@att.net>
2024-03-07 17:11:52 +0000

    Create 2dcameraClass.h

---
+++ include/camera/2dcameraClass.h
@@ -0,0 +1,39 @@
+#ifndef CAMERA_CLASS_H
+#define CAMERA_CLASS_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+#include "shaders/shaderClass.h"
+
+class Camera
+{
+    public:
+        glm::vec3 Position;
+        glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+        glm::mat4 cameraMatrix = glm::mat4(1.0f);
+        //Prevents mouse jumping when first clicking
+        bool firstClick = true;
+
+        //Stores width and height of window
+        int width;
+        int height;
+
+        //Stores camera speed when using keyboard, and sensitivity when using mouse
+        float speed = 0.01f;
+        float sensitivity = 100.0f;
+
+        Camera(int width, int height, glm::vec3 position);
+
+        void updateMatrix(float FOVdegree, float nearPlane, float farPlane);
+        void Matrix(Shader& shader, const char* uniform);
+        void Inputs(GLFWwindow* window);
+};
+
+#endif

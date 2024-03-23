defdd0d6c105404df8768914b4d8366cfd5a6fc9
Josh Rutledge <joshua.rutledge@att.net>
2024-03-23 13:02:48 +0000

    Create 2dVBO.h

---
+++ include/shaderBuffers/2dVBO.h
@@ -0,0 +1,26 @@
+#ifndef VBO_CLASS_H
+#define VBO_CLASS_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+struct Vertex 
+{
+    glm::vec3 position;
+    glm::vec3 color;
+    glm::vec2 texUV;
+};
+
+class VBO
+{
+    public:
+        GLuint ID;
+        VBO();
+        void Construct(std::vector<Vertex>& vertices);
+        void Bind();
+        void Unbind();
+        void Delete();
+};
+
+#endif
